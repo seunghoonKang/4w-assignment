f25224b7b5674c249424fa8a9ce73f012d481eb5
seunghoon <tmdgns4321@gmail.com>
2022-10-10 12:01:33 +0900

    todo 인풋 구현중..

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function AddTodo() {
-  return <div>AddTodo</div>;
+  return (
+    <section>
+      <form action="">
+        <label htmlFor="title">제목</label>
+        <input id="title"></input>
+        <label htmlFor="content">내용</label>
+        <input id="content"></input>
+        <button>추가하기</button>
+      </form>
+    </section>
+  );
 }
